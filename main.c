b7609dda8ce6a49f49edcfb9683bacdd020559ab
danicoln <lincolndeveloper@outlook.com>
2022-11-23 15:11:01 -0300

    Inclusão do método endCompleto() e manutenção do método cadastrarPaciente()

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <locale.h>
+#include <wchar.h>
 
 /*===============================================================*/
 /*Sistema de cadastro de pacientes diagnosticados com Covid-19*/
@@ -52,25 +53,53 @@ int calculaIdade(rgData nasc, rgData hoje)
     idade = hoje.ano - nasc.ano;
     if((hoje.mes < nasc.mes) || ((hoje.mes == nasc.mes) && (hoje.dia < nasc.dia)))
         idade = idade - 1;
-    if(idade >= 65)
-    {
-        printf("O paciente pertence ao grupo de risco!\n");
-
-    }
-    else
-    {
-        printf("O paciente NÃO pertence ao grupo de risco.\n");
-
-    }
 
     return (idade);
 }
 
+/*=================================================================================*/
+// struct endereço
+/*=================================================================================*/
 
+typedef struct {
+	char rua;
+	int numero;
+	char bairro;
+	char cidade;
+	char estado;
+}endereco;
+
+/*=================================================================================*/
+// método de endereço completo
+/*=================================================================================*/
+endereco endCompleto(){
+
+    endereco dados;
+    char rua, bairro, cidade, estado;
+	int numero;
+	fflush(stdin);
+    printf("Rua: ");
+    scanf("%s", &dados.rua);
+    fflush(stdin);
+    printf("\n Informe o numero: \n");
+    scanf("%i", &dados.numero);
+    fflush(stdin);
+   printf("\n Informe o bairro: \n");
+   	scanf("%s", &dados.bairro);
+   	fflush(stdin);
+    printf("\n Informe a cidade: \n");
+    scanf("%s", &dados.cidade);
+    fflush(stdin);
+    printf("\n Informe o estado: \n");
+    scanf("%s", &dados.estado);
+    fflush(stdin);
+
+    return dados;
+}
 /*=================================================================================*/
 // struct Paciente
 /*=================================================================================*/
-typedef struct Paciente  // tipo de dados
+typedef struct Paciente // tipo de dados
 {
     char nome[30]; // n deu pra definir o array porque o método fopen() não está aceitando
     char cpf[11];
@@ -78,28 +107,23 @@ typedef struct Paciente  // tipo de dados
     rgData niver;
     char email[20];
     char cep[9];
+    endereco enderCompl;
+    char comorbidade;
     //time_t dataDiagnostico; // verificar a função
 };
-
-
 
 /*=================================================================================*/
 // função para cadastro de paciente
 /*=================================================================================*/
-void cadastrarPaciente()
-{
+void cadastrarPaciente(){
     FILE *arquivo;
 
     char opcao;
-
 
     struct Paciente paciente;
 
     printf("Nome do paciente: ");
     scanf("%s",&paciente.nome);
-    //arquivo = fopen(paciente.nome, "w"); // aqui eu nomeio o arquivo
-
-    // fprintf(arquivo, &paciente.nome, "w"); // adiciona o nome no arquivo
 
     if (arquivo == NULL)
     {
@@ -116,18 +140,17 @@ void cadastrarPaciente()
 
         arquivo = fopen(&paciente.nome, "w"); // nomeia o arquivo
 
+        fflush(stdin);
         printf("\nDigite o cpf do paciente: ");
         scanf("%s", &paciente.cpf);
-
+        fflush(stdin);
         printf("Informe o telefone:");
         scanf("%s", &paciente.telefone);
-
-
+        fflush(stdin);
 
         printf("\nInforme a data de nascimento: \n");
         printf("\n > Formato: ++/++/++++ \n\n > Nascimento: ");
         scanf("%s", &paciente.niver);
-
 
         printf("\nInforme o email: \n");
         scanf("%s", &paciente.email);
@@ -136,6 +159,22 @@ void cadastrarPaciente()
         printf("\n > Formato: *****-*** \n\n > CEP: ");
         scanf("%s", &paciente.cep);
 
+	/*	printf("\nInforme o endereço Completo: \n");
+        endCompleto();
+*/
+		int opc;
+      	char msg;
+    	bool comorbidade = true;
+        printf("\nO paciente possui comorbidade? (1. Sim / 2. Não)\n");
+        scanf("%i", &opc);
+        if(opc == 1){
+        printf("\nInforme a comorbidade\n");
+	   	scanf("%s", &msg);
+        }
+        else{
+            printf("\nO paciente não possui comorbidade.\n");
+            comorbidade = false;
+        }
 
         printf("\n  |       Nome      ---------- : %s", &paciente.nome);
         printf("\n  |       CPF       ---------- : %s", &paciente.cpf);
@@ -145,22 +184,32 @@ void cadastrarPaciente()
         printf("\n  |       CEP       ---------- : %s", &paciente.cep);
 
 
-        fprintf(arquivo, "\n    #==================== Dados do paciente ====================#");
-        fprintf(arquivo, "\n    |       Nome      ---------- : %s", &paciente.nome);
-        fprintf(arquivo, "\n    |       CPF       ---------- : %s", &paciente.cpf);
-        fprintf(arquivo, "\n    |       Telefone  ---------- : %s", &paciente.telefone);
-        fprintf(arquivo, "\n    |       DataNasc  ---------- : %s", &paciente.niver);
-        fprintf(arquivo, "\n    |       Email     ---------- : %s", &paciente.email);
-        fprintf(arquivo, "\n    |       CEP       ---------- : %s", &paciente.cep);
-
         rgData dtNasc, dtDiagnostico;
-        dtDiagnostico = entrada("\n\nPara salvar a idade do paciente por favor, insira novamente:\nData do diagnóstico:   \n\n");
-        dtNasc = entrada("Data de nascimento: ");
-        fprintf(arquivo, "\nO paciente tem %d ano(s).", calculaIdade(dtNasc, dtDiagnostico));
+        if((comorbidade = false) && (calculaIdade(dtNasc, dtDiagnostico) <65)){
 
+        printf("\nO paciente NÃO pertence ao grupo de risco.\n");
 
         fclose(arquivo);
         return 0;
+        }else{
+
+        fprintf(arquivo, "\n    #==================== Dados do paciente ====================#");
+        /*fprintf(arquivo, "\n    |       Nome      ---------- : %s", &paciente.nome);
+        fprintf(arquivo, "\n    |       CPF       ---------- : %s", &paciente.cpf);
+        fprintf(arquivo, "\n    |       Telefone  ---------- : %s", &paciente.telefone);
+        fprintf(arquivo, "\n    |       DataNasc  ---------- : %s", &paciente.niver);
+        fprintf(arquivo, "\n    |       Email     ---------- : %s", &paciente.email); */
+        fprintf(arquivo, "\n    |       CEP       ---------- : %s", &paciente.cep);
+
+        dtDiagnostico = entrada("\n\nPara confirmar, insira a data do diagnóstico:   \n\n");
+        dtNasc = entrada("Data de nascimento: ");
+        fprintf(arquivo, "\n    |       Idade do paciente -- : %d.", calculaIdade(dtNasc, dtDiagnostico));
+
+        fprintf(arquivo, "\n    #========================================================#");
+			}
+
+        fclose(arquivo);
+         return 0;
     }
 
 }
@@ -170,8 +219,6 @@ void cadastrarPaciente()
 /*===============================================================*/
 typedef struct
 {
-
-
     char login[30]; // vetor login da struct pessoa
 
     char senha[30]; // vetor senha da struct pessoa
@@ -190,6 +237,9 @@ int main()
     /*===============================================================*/
 
 
+
+printf("Bem vindo ao sistema!\n\n\n");
+
         char login[30]; // responsável por armazenar a senha inserida pelo usuário
 
         char senha[30]; // responsável por armazenar a senha inserida pelo usuário
@@ -198,14 +248,13 @@ int main()
 
         strcpy(p[0].senha, "123456");
 
-        printf("\nlogin:");
-
+        printf("\n===============================================================\n");
+        printf("login:");
         scanf("%s", login); // armazenando os dados inseridos pelo usuário para o vetor login que está dentro da função main
-
-        printf("\nsenha:");
-
+        printf("\n===============================================================\n");
+        printf("senha:");
         scanf("%s", senha); // armazenando os dados inseridos pelo usuário para o vetor senha que está dentro da função main
-
+        printf("\n===============================================================\n");
         if ((strcmp(login,p[0].login)==0) && (strcmp(senha,p[0].senha)==0))  // A função strcmp é responsável por comparar string com string
         {
 
@@ -241,7 +290,6 @@ int main()
         printf("\n  |                                                                                             |");
         printf("\n  |                                                                                             |");
         printf("\n  |                                                                                             |");
-        printf("\n  |                                                                                             |");
         printf("\n  |           1 - Cadastrar                                                                     |");
         printf("\n  |           0 - Sair                                                                          |");
         printf("\n  |                                                                                             |");
@@ -255,10 +303,10 @@ int main()
         printf("\n  |                                                                                             |");
         printf("\n  |                                                                                             |");
         printf("\n  |           Gerente de Projetos:        Ricardo Bastos Dominguez       RA: 0606660            |");
-        printf("\n  |           Engº de Software:           Luan Piaulino Silva            RA: 0608180            |");
+        printf("\n  |           Engº de Software:           Daniel Lincoln M. F. Silva     RA: 0616297            |");
         printf("\n  |           Arqº de Software:           Henrique Alves de Lima         RA: 0612163            |");
-        printf("\n  |           Analista de Requisitos:     Gabriel                        RA: 0000000            |");
-        printf("\n  |           Desenvolvedor:              Rafael                         RA: 0000000            |");
+        printf("\n  |           Analista de Requisitos:     Gabriel Tavares Lopes          RA: 0610868            |");
+        printf("\n  |           Desenvolvedor:              Rafael de Assis Oliveira       RA: 0600717            |");
         printf("\n  |           Desenvolvedor:              Daniel Lincoln M. F. Silva     RA: 0616297            |");
         printf("\n  |                                                                                             |");
         printf("\n  #=============================================================================================#\n");
@@ -271,14 +319,7 @@ int main()
 
         case 1:
 
-
             cadastrarPaciente();
-
-            break;
-
-        case 2:
-
-
 
             break;
 
@@ -296,4 +337,3 @@ int main()
 
     return 0;
 }
-
